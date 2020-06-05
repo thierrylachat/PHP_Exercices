7bd9d328a06a81d04e3d1d33306de31a695587bf
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-05 14:26:28 +0200

    Partie 1 _ Exercice 8

---
+++ Partie 1 - Les variables/Exercice 8.php
@@ -0,0 +1,20 @@
+<!-- Création de variables et insertion du header et de la barre de navigation. -->
+
+<?php
+    $x= 3+4;
+    $y= 5*20;
+    $z= 45/5;
+    $titre= 'Exercice 8';
+    include 'header.php';
+    // include 'navbar.php';
+?>
+
+<!-- Affichage des valeurs x, y et z.  -->
+
+<p><?= $x;?></p>
+<p><?= $y;?></p>
+<p><?= $z;?></p>
+
+<!-- Insertion du footer. -->
+
+<?php include 'footer.php';
