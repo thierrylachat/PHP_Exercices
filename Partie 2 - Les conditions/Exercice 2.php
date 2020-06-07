35506d9c0a7436c83d7ffc2b96bb417b33309a8b
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-07 22:20:47 +0200

    Partie 2 - Exercice 2.

---
+++ Partie 2 - Les conditions/Exercice 2.php
@@ -0,0 +1,31 @@
+<!-- Création de variables et insertion du header et de la barre de navigation. -->
+
+<?php
+    $isEasy= true;
+    $titre= 'Exercice 2';
+    include 'header.php';
+?>
+
+<!-- Afficher "C'est facile !!" si c'est vrai. Dans le cas contraire afficher "C'est difficile !!!". -->
+
+<p>
+    <?php if ($isEasy == true) {
+        echo "C'est facile !!";
+    } else {
+        echo "C'est difficile !!!";
+    }
+    ?>
+</p>
+
+<p>
+    <?php if ($isEasy != true) {
+        echo "C'est difficile !!!";
+    } else {
+        echo "C'est facile !!";
+    }
+    ?>
+</p>
+
+<!-- Insertion du footer. -->
+
+<?php include 'footer.php'; ?>
