a2c0778442d55bf1663cd42026b1f28fa715150d
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-07 22:02:28 +0200

    Partie 2 - Exercice 1.

--- Partie 2 - Les conditions/Exercice 1.php
+++ Partie 2 - Les conditions/Exercice 1.php
@@ -1,4 +1,4 @@
-<!-- Initialisation de variables et insertion du header et de la barre de navigation. -->
+<!-- Création de variables et insertion du header et de la barre de navigation. -->
 
 <?php
     $age= 15;
@@ -6,17 +6,17 @@
     include 'header.php';
 ?>
 
-<!-- Affichage du message si l'âge est supérieur ou égale à 18", afficher Vous êtes majeur. Dans le cas contraire, afficher "Vous êtes mineur". -->
+<!-- Affichage du message "Vous êtes majeur" si l'âge est supérieur ou égal à 18. Dans le cas contraire, afficher "Vous êtes mineur". -->
 
 <p>
-if ($age < 18) 
-{
-    echo 'Vous êtes majeur !';
-} else {
-    echo 'Vous êtes mineur !';
-}
+    <?php if ($age > 18) {
+        echo 'Vous êtes majeur !';
+    } else {
+        echo 'Vous êtes mineur !';
+    }
+    ?>
 </p>
 
 <!-- Insertion du footer. -->
 
-<?php include 'footer.php';
+<?php include 'footer.php'; ?>
