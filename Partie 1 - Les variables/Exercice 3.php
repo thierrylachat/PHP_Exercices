b4ba87d259151cac461a142a1642fac4be034c54
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-07 20:18:40 +0200

    Partie 1 - Les variables - Exercice 3 OK.

--- Partie 1 - Les variables/Exercice 3.php
+++ Partie 1 - Les variables/Exercice 3.php
@@ -1,18 +1,24 @@
-<!-- Initialisation de variables et insertion du header et de la barre de navigation. -->
+<!-- Création de variables et insertion du header et de la barre de navigation. -->
 
 <?php
-    $km= '1';
+    $km= 1;
     $titre= 'Exercice 3';
     include 'header.php';
 ?>
 
-<!-- Affichage de la variable km et changement successive de la valeur. -->
+<!-- Affichage de la variable km. -->
 
-<p> <?= $km.'km' ?> </p>
-<?php $km= '3'; ?>
-<p> <?= $km.'km' ?> </p>
-<?php $km= '125'; ?>
-<p> <?= $km.'km' ?> </p>
+<p> <?= $km.'km'; ?> </p>
+
+<!-- Changement de la variable par la valeur 3. -->
+
+<?php $km= 3; ?>
+<p> <?= $km.'km'; ?> </p>
+
+<!-- Changement de la variable par la valeur 125. -->
+
+<?php $km= 125; ?>
+<p> <?= $km.'km'; ?> </p>
 
 <!-- Insertion du footer. -->
 
