7c940a340d16ad52bf8a7fa5f0a8216b8af8cdf9
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-08 23:08:33 +0200

    Partie 2 - Exercice 8.

--- Partie 2 - Les conditions/Exercice 8.php
+++ Partie 2 - Les conditions/Exercice 8.php
@@ -1,13 +1,39 @@
-<!-- Création de variable et insertion du header et de la barre de navigation. -->
-
-<?php
-    $isOk = true;
-    $titre= 'Exercice 8';
-    include 'header.php';
-?>
-
 <!-- Traduire ce code avec des if et des else :  
 
+    <?php
+     echo ($isOk) ? 'c\'est ok !!' : 'c\'est pas bon !!!';
+    ?> -->
+
+
+<!-- Création de variable et insertion du header et de la barre de navigation. -->
+    
+<?php
+
+    $titre= 'Exercice 8';
+    include 'header.php';
+
+    $isOk = true;
+    if ($isOk == true)
+        {
+            $message = "C'est OK !";
+        }
+        else
+        {
+           $message = "Ce n'est pas bon !";
+        }  
+?>
+
+<!-- 1° solution : affichage du message dans un paragraphe. -->
+
+<p><?= $message ?></p>
+
+<!-- 2° solution : alternance php et html. -->
+
+<?php if ($isOk == true) { ;?>
+    <p>C'est OK !</p>
+<?php } else { ;?>
+    <p>Ce n'est pas bon !</p>
+<?php } ;?>
 
 
 <!-- Insertion du footer. -->
