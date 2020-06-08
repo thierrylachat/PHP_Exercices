b91a949c904e28b0a58437e47fb7393d39d2c987
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-08 21:50:12 +0200

    Partie 2 - Les conditions - Exercice 3 OK.

--- Partie 2 - Les conditions/Exercice 3.php
+++ Partie 2 - Les conditions/Exercice 3.php
@@ -1,8 +1,8 @@
 <!-- Création de variables et insertion du header et de la barre de navigation. -->
 
 <?php
-    $age= 13;
-    $gender= 'homme';
+    $age= rand(1,120);
+    $genre = substr(str_shuffle('FH'),0,1);
     $titre= 'Exercice 3';
     include 'header.php';
 ?>
@@ -13,41 +13,30 @@ Vous êtes un homme et vous êtes mineur
 Vous êtes une femme et vous êtes majeur
 Vous êtes une femme et vous êtes mineur  -->
 
-<p>
-    <?php 
-    if ($gender == 'homme' and $age > 18) {
-        echo "Vous êtes un homme et vous êtes majeur.";
-    } elseif ($gender == 'homme' and $age < 18) {
-        echo "Vous êtes un homme et vous êtes mineur.";
-    } elseif ($gender == 'femme' and $age > 18) {
-        echo "Vous êtes une femme et vous êtes majeur.";
-    } else {
-        echo "Vous êtes une femme et vous êtes mineur.";
-    }
-    ?>
-</p>
+    <?php if ($age < 18 && $genre == 'F') : ?>
+        <p>Vous êtes une femme et vous êtes mineur.</p>
+    <?php  elseif ($age >= 18 && $genre =='F') : ?>
+        <p>Vous êtes une femme et vous êtes majeur.</p>
+    <?php  elseif ($age < 18 && $genre == 'H') : ?>
+        <p>Vous êtes un homme et vous êtes mineur.</p>
+    <?php  elseif ($age >= 18 && $genre == 'H') : ?>
+        <p>Vous êtes un homme et vous êtes majeur.</p>
+    <?php endif; ?>
+
+
+<!-- Deuxième solution qui élimine les possibilités. -->
+
+    <?php if ($age < 18 && $genre == 'F') : ?>
+        <p>Vous êtes une femme et vous êtes mineur.</p>
+    <?php  elseif ($age < 18) : ?>
+        <p>Vous êtes un homme et vous êtes mineur.</p>
+    <?php  elseif ($genre == 'F') : ?>
+        <p>Vous êtes une femme et vous êtes majeur.</p>
+    <?php  else : ?>
+        <p>Vous êtes un homme et vous êtes majeur.</p>
+    <?php endif; ?>
+
 
 <!-- Insertion du footer. -->
 
 <?php include 'footer.php'; ?>
-
-<!-- Deuxième solution -->
-
-<?php
-    $age= rand(1,120);
-    $genre = substr(str_shuffle('FH'),0,1);
-?>
-
-<?php if ($age <= 18 && $genre == 'F') : ?>
-    <p>Vous êtes une femme et vous êtes mineur.</p>
-    <?php elseif ($age >= 18 && $genre == 'F') : ?>
-    <p>Vous êtes une femme et vous êtes majeur.</p>
-    <?php elseif ($age <= 18 && $genre == 'H') : ?>
-    <p>Vous êtes une homme et vous êtes mineur.</p>
-    <?php elseif ($age >= 18 && $genre == 'H') : ?>
-    <p>Vous êtes un homme et vous êtes majeur.</p>
-    <?php endif; ?>
-
-<!-- Troisième solution -->
-
-Elimnation des conditions au fur et à mesure...
