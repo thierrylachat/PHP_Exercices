f266f640ba7aebab399d74b4bf206b73d8fd2117
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-08 21:19:50 +0200

    Partie 2 - Les conditions - Exercice 1 OK.

--- Partie 2 - Les conditions/Exercice 1.php
+++ Partie 2 - Les conditions/Exercice 1.php
@@ -1,48 +1,33 @@
 <!-- Création de variables et insertion du header et de la barre de navigation. -->
 
 <?php
-    $age= 15;
-    $titre= 'Exercice 1';
-    include 'header.php';
-?>
-
-<!-- Affichage du message "Vous êtes majeur" si l'âge est supérieur ou égal à 18. Dans le cas contraire, afficher "Vous êtes mineur". -->
-
-<p>
-    <?php if ($age > 18) {
-        echo 'Vous êtes majeur !';
-    } else {
-        echo 'Vous êtes mineur !';
-    }
-    ?>
-</p>
-
-<!-- Insertion du footer. -->
-
-<?php include 'footer.php'; ?>
-
-
-<!-- Deuxième solution -->
-<?php
     $age= 45;
     $message= '';
-    if ($age > 18) {
+    if ($age >= 18) {
         $message = 'Vous êtes majeur !';
     } else {
         $message = 'Vous êtes mineur !';
     }
+    $titre= 'Exercice 1';
+    include 'header.php';
 ?>
+
+
+<!-- Première solution avec affichage du message "Vous êtes majeur" si l'âge est supérieur ou égal à 18. Dans le cas contraire, afficher "Vous êtes mineur". -->
 
 <p><?= $message; ?></p>
 
-<!-- Troisième solution pas besoin du echo...-->
-<?php
-$age = 32;
-?>
+
+<!-- Deuxième solution avec inscription des messages dans des paragraphes-->
 
 <?php
 if ($age >= 18 ) { ?> 
     <p>Vous êtes majeur !</p>
 <?php } else { ?> 
     <p>Vous êtes mineur !</p>
-<?php }
+<?php } ?>
+
+
+<!-- Insertion du footer. -->
+
+<?php include 'footer.php'; ?>
