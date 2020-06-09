f95393db5d1cd8eebe81e0b977e19e86587b7500
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-09 21:46:29 +0200

    Partie 3 - Les boucles - Exercice 7 OK

--- Partie 3 - Les boucles/Exercice 7.php
+++ Partie 3 - Les boucles/Exercice 7.php
@@ -1,4 +1,4 @@
-<!-- En allant de 1 à 100 avec un pas de 15, afficher le message On tient le bon bout. -->
+<!-- En allant de 1 à 100 avec un pas de 15, afficher le message "On tient le bon bout !". -->
 
 
 <!-- Création de variables et insertion du header et de la barre de navigation. -->
@@ -8,12 +8,11 @@
     include 'header.php';
 ?>
 
+<!-- Création d'une boucle For -->
 
-<!-- Première solution avec affichage du message "Vous êtes majeur" si l'âge est supérieur ou égal à 18. Dans le cas contraire, afficher "Vous êtes mineur". -->
-
-<?php for($step = 1; $step <= 15; $step ++ ) {?>
-    <p><?php echo 'On y arrive presque'; ?> </p>
-<?php } ?>
+<?php for ($step = 1; $step <= 100 ; $step += 15){ ?>
+    <p> <?php echo 'On tient le bon bout !'; ?> </p> 
+<?php } ;?>
 
 
 <!-- Insertion du footer. -->
