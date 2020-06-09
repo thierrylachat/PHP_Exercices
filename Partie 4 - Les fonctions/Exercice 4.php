f3bcf4e9f23fca4d24ab5c4c2e072800b37db644
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-09 22:18:46 +0200

    Partie 4 - Les fonctions - Exercices 1, 2 3 et 4 OK.

--- Partie 4 - Les fonctions/Exercice 4.php
+++ Partie 4 - Les fonctions/Exercice 4.php
@@ -1,9 +1,9 @@
 <!-- Enoncé : 
 Faire une fonction qui prend en paramètre deux nombres. 
 La fonction doit retourner :
-Le premier nombre est plus grand si le premier nombre est plus grand que le deuxième
-Le premier nombre est plus petit si le premier nombre est plus petit que le deuxième
-Les deux nombres sont identiques si les deux nombres sont égaux -->
+Le premier nombre est plus grand si le premier nombre est plus grand que le deuxième.
+Le premier nombre est plus petit si le premier nombre est plus petit que le deuxième.
+Les deux nombres sont identiques si les deux nombres sont égaux. -->
 
 
 <!-- Création de variables et insertion du header et de la barre de navigation. -->
