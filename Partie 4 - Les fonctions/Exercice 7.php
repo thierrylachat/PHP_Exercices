eec6f1f6f7c0dfabc3b8635d5bc98e81ba90a67c
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-09 22:35:15 +0200

    Partie 4 - Les fonctions - Exercice 7 OK.

--- Partie 4 - Les fonctions/Exercice 7.php
+++ Partie 4 - Les fonctions/Exercice 7.php
@@ -1,13 +1,11 @@
 <!-- Enoncé : 
 Faire une fonction qui prend deux paramètres : l'âge et le genre d'une personne. 
-Le genre peut être : Homme Femme.
-
+Le genre peut être : Homme ou Femme.
 La fonction doit renvoyer en fonction des paramètres :
-Vous êtes un homme et vous êtes majeur
-Vous êtes un homme et vous êtes mineur
-Vous êtes une femme et vous êtes majeure
-Vous êtes une femme et vous êtes mineure
-
+Vous êtes un homme et vous êtes majeur.
+Vous êtes un homme et vous êtes mineur.
+Vous êtes une femme et vous êtes majeur.
+Vous êtes une femme et vous êtes mineur.
 Gérer tous les cas. -->
 
 
@@ -21,15 +19,25 @@ Gérer tous les cas. -->
 
 <!-- Création de la fonction profile. -->
 
-<?php function profile($gender, $age) {
-    return 'Vous êtes un '.$gender.' et vous êtes '.$age.'.';
-} ?>
-
+<?php
+    function profile($gender, $age){
+        $message = '';
+        if ($gender == 'F' && $age >= 18) {
+            $message = 'Vous êtes une femme et vous êtes majeur.';
+        }elseif ($gender == 'H' && $age >= 18) {
+            $message = 'Vous êtes un homme et vous êtes majeur.';
+        }elseif ($gender == 'F') {
+            $message = 'Vous êtes une femme et vous êtes mineur.';
+        }else {
+            $message = 'Vous êtes un homme et vous êtes mineur.';
+        }
+        return $message;
+    }
+?>
 
 <!-- Affichage de la concaténation avec appel de la fonction profile. -->
 
-<p><?= profile(substr(str_shuffle('FH'), 0, 1)), ran(1,30); ?></p>
-
+<p><?= profile(substr(str_shuffle('FH'), 0, 1), rand(1,30)) ?></p>
 
 <!-- Insertion du footer. -->
 
