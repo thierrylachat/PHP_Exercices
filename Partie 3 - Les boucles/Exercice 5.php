28d6c052e32cb26217e9d7fe796961ff37af2733
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-09 21:31:18 +0200

    Partie 3 - Les boucles - Exercice 5 OK.

--- Partie 3 - Les boucles/Exercice 5.php
+++ Partie 3 - Les boucles/Exercice 5.php
@@ -1,4 +1,4 @@
-<!-- En allant de 1 à 15 avec un pas de 1, afficher le message On y arrive presque. -->
+<!-- En allant de 1 à 15 avec un pas de 1, afficher le message "On y arrive presque". -->
 
 
 <!-- Création de variables et insertion du header et de la barre de navigation. -->
@@ -9,10 +9,10 @@
 ?>
 
 
-<!-- Première solution avec affichage du message "Vous êtes majeur" si l'âge est supérieur ou égal à 18. Dans le cas contraire, afficher "Vous êtes mineur". -->
+<!-- Création d'une boucle For -->
 
-<?php for($step = 1; $step <= 15; $step ++ ) {?>
-    <p><?php echo 'On y arrive presque'; ?> </p>
+<?php for ($step = 1; $step <= 15; $step ++ ) {?>
+    <p><?php echo 'On y arrive presque !'; ?> </p>
 <?php } ?>
 
 
