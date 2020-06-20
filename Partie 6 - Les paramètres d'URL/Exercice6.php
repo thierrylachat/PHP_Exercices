1ce600adea96f77847d3a6b77ed59a5c36642d4e
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-20 23:26:30 +0200

    Partie 6 - Les paramètres d'URL - Exercice 6 OK.

--- Partie 6 - Les paramètres d'URL/Exercice6.php
+++ Partie 6 - Les paramètres d'URL/Exercice6.php
@@ -4,15 +4,15 @@ Tester sur cette page que tous les paramètres de cette URL existent  et les aff
 <!-- Création de variables et insertion du header et de la barre de navigation. -->
 
 <?php
-    $titre= 'Exercice6';
-    include 'header.php';
+$titre= 'Exercice6';
+include 'header.php';
 ?>
 
 
 <!-- Test et affichage du paramètre week de l'URL. -->
 
-<p> <?= $_GET["building"]; ?></p>
-<p> <?= $_GET["room"]; ?></p>
+<p> <?= $_GET["building"]; ?> </p>
+<p> <?= $_GET["room"]; ?> </p>
 
 
 <!-- Insertion du footer. -->
