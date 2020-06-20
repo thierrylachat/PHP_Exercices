2bdbba7c358435ac4a67998bed281f5741222b9a
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-20 22:50:33 +0200

    Partie 5 - Les tableaux - Exercice 10 OK.

--- Partie 5 - Les tableaux/Exercice 10.php
+++ Partie 5 - Les tableaux/Exercice 10.php
@@ -1,27 +1,56 @@
-<!-- Enoncé : avec le tableau de l'exercice 5, afficher toutes les valeurs de ce tableau ainsi que les clés associés.  
+<!-- Enoncé : avec le tableau de l'exercice 5, afficher toutes les valeurs de ce tableau ainsi que les clés associés.
 Cela pourra être, par exemple, de la forme : Le département + nom du département + a le numéro + numéro du département  -->
 
 
 <!-- Création de variables et insertion du header et de la barre de navigation. -->
 
 <?php
-    $titre= 'Exercice 10';
-    include 'header.php';
+$titre = 'Exercice 10';
+include 'header.php';
 ?>
 
 
 <!-- Création du tableau associatif. -->
 
-<?php $departments = array("80"=>"Somme", "60"=>"Oise", "02"=>"Aisne", "62"=>"Pas-De-Calais", "59"=>"Nord"); ?>
+<?php $departments = array("80" => "Somme", "60" => "Oise", "02" => "Aisne", "62" => "Pas-De-Calais", "59" => "Nord");?>
 
 
 <!-- Affichage du tableau associatif. -->
-<?php foreach($departments as $departmentNumber => $department) {
-  echo "Le département " . $department . " a le numéro " . $departmentNumber . "." . "</br>";
-}
-?>
+<?php foreach ($departments as $departmentNumber => $department) {
+    echo "Le département " . $department . " a le numéro " . $departmentNumber . "." . "</br>";
+}; ?>
 
+<br>
+
+
+<!-- Affichage sous la forme d'un tableau. -->
+
+<div class="row">
+    <div class="col-md-6">
+
+        <table class="table table-bordered table-striped">
+            <thead class="bg-info">
+                <tr>
+                    <th>Numéro de département</th>
+                    <th>Nom du département</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
+                foreach ($departments as $departmentNumber => $department) { ;?>
+                <tr>
+                    <td><?=$departmentNumber?></td>
+                    <td><?=$department?></td>
+                </tr>
+                <?php } ;?>
+
+            </tbody>
+        </table>
+
+    </div>
+</div>
 
 <!-- Insertion du footer. -->
 
-<?php include 'footer.php'; ?>
+<?php include 'footer.php';?>
