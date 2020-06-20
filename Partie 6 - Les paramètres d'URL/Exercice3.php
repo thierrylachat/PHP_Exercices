ebd36a6183d91bbc72ebd4efb61bf7dc4c3708d5
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-20 23:19:08 +0200

    Partie 6 - Les paramètres d'URL - Exercice 3 OK.

--- Partie 6 - Les paramètres d'URL/Exercice3.php
+++ Partie 6 - Les paramètres d'URL/Exercice3.php
@@ -1,21 +1,21 @@
-<!-- Enoncé : faire une page index.php. 
+<!-- Enoncé : faire une page index.php.
 Tester sur cette page que tous les paramètres de cette URL existent et les afficher : index.php?startDate=2/05/2016&endDate=27/11/2016 -->
 
 
 <!-- Création de variables et insertion du header et de la barre de navigation. -->
 
 <?php
-    $titre= 'Exercice3';
-    include 'header.php';
+$titre = 'Exercice3';
+include 'header.php';
 ?>
 
 
 <!-- Test et affichage des paramètres name et firstname de l'URL. -->
 
-<p> <?= $_GET["startDate"]; ?></p>
-<p> <?= $_GET["endDate"]; ?></p>
+<p> <?= $_GET["startDate"] ;?> </p>
+<p> <?= $_GET["endDate"] ;?> </p>
 
 
 <!-- Insertion du footer. -->
 
-<?php include 'footer.php'; ?>
+<?php include 'footer.php';?>
