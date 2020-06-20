c6a452713021693d58f20067e0475b95983a9380
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-20 23:22:07 +0200

    Partie 6 - Les paramètres d'URL - Exercice 4 OK.

--- Partie 6 - Les paramètres d'URL/Exercice4.php
+++ Partie 6 - Les paramètres d'URL/Exercice4.php
@@ -1,21 +1,21 @@
 <!-- Enoncé : Faire une page index.php.
-Tester sur cette page que tous les paramètres de cette URL existent  et les afficher : index.php?language=PHP&server=LAMP -->
+Tester sur cette page que tous les paramètres de cette URL existent et les afficher : index.php?language=PHP&server=LAMP -->
 
 
 <!-- Création de variables et insertion du header et de la barre de navigation. -->
 
 <?php
-    $titre= 'Exercice4';
-    include 'header.php';
+$titre = 'Exercice4';
+include 'header.php';
 ?>
 
 
 <!-- Test et affichage des paramètres name et firstname de l'URL. -->
 
-<p> <?= $_GET["language"]; ?></p>
-<p> <?= $_GET["server"]; ?></p>
+<p> <?= $_GET["language"] ;?></p>
+<p> <?= $_GET["server"] ;?></p>
 
 
 <!-- Insertion du footer. -->
 
-<?php include 'footer.php'; ?>
+<?php include 'footer.php';?>
