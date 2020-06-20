9b1b474c06422e2160db902ae186d8211462227e
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-20 22:29:15 +0200

    Partie 5 - Les tableaux - Exercice 8 OK.

--- Partie 5 - Les tableaux/Exercice 8.php
+++ Partie 5 - Les tableaux/Exercice 8.php
@@ -4,26 +4,54 @@
 <!-- Création de variables et insertion du header et de la barre de navigation. -->
 
 <?php
-    $titre= 'Exercice 8';
-    include 'header.php';
+$titre = 'Exercice 8';
+include 'header.php';
 ?>
 
 
 <!-- Création du tableau months. -->
 
-<?php 
-    $months = array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre");
-    $arrlength = count($months);
-?>  
+<?php
+$months = array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre");
+$arrlength = count($months);
+?>
 
 
 <!-- Utilisation d'une boucle For pour affichage du tableau. -->
 <?php for ($monthsIndex = 0; $monthsIndex < $arrlength; $monthsIndex++) {
-        echo $months[$monthsIndex] .'<br />';
-        } 
-?>
+    echo $months[$monthsIndex] . '<br />';
+}; ?>
+
+
+<!-- Affichage sous la forme d'un tableau. -->
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <table class="table table-bordered table-striped">
+                <thead class="bg-info">
+                    <tr>
+                        <th>N° Mois</th>
+                        <th>Nom du mois</th>
+                    </tr>
+                </thead>
+                <tbody>
+                
+                    <?php
+                    $limit = count($months);
+                    for ($monthsIndex = 0; $monthsIndex < $limit; $monthsIndex++) {?>
+                    <tr>
+                        <td> <?= $monthsIndex + 1 ;?> </td>
+                        <td> <?= $months[$monthsIndex] ;?> </td>
+                    </tr>
+                    <?php 
+                    }; ?>
+
+                </tbody>
+            </table>
+        </div>
 
 
 <!-- Insertion du footer. -->
 
-<?php include 'footer.php'; ?>
+<?php include 'footer.php';?>
