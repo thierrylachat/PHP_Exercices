762f1527407cf6bbbfb1c5dbb1f1c585e7bfdbbf
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-20 21:54:57 +0200

    Partie 5 - Les tableaux - Exercice 5 OK.

--- Partie 5 - Les tableaux/Exercice 5.php
+++ Partie 5 - Les tableaux/Exercice 5.php
@@ -3,23 +3,24 @@
 <!-- Création de variables et insertion du header et de la barre de navigation. -->
 
 <?php
-    $titre= 'Exercice 5';
-    include 'header.php';
+$titre = 'Exercice 5';
+include 'header.php';
 ?>
 
 
 <!-- Création du tableau associatif. -->
 
-<?php $departments = array("80"=>"Somme", "60"=>"Oise", "02"=>"Aisne", "62"=>"Pas-De-Calais", "59"=>"Nord"); ?>
+<?php $departments = array("80" => "Somme", "60" => "Oise", "02" => "Aisne", "62" => "Pas-De-Calais", "59" => "Nord") ;?>
+<?php $departments = ['02' => 'Aisne', '59' =>'Nord', '60' => 'Oise', '80' => 'Somme', '62' => 'Pas-de-calais'] ;?>
 
 
 <!-- Affichage du tableau. -->
 
 <?php foreach ($departments as $element) {
-    echo $element. '<br>'; }
-    ?>
+    echo $element . '<br>';}
+?>
 
 
 <!-- Insertion du footer. -->
 
-<?php include 'footer.php'; ?>
+<?php include 'footer.php';?>
