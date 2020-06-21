678a05291ef579b759cda1bab028f10f0a51dfe9
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-21 22:13:28 +0200

    Partie 7 - Les formulaires - Exercice 5 OK.

--- Partie 7 - Les formulaires/exercice5.php
+++ Partie 7 - Les formulaires/exercice5.php
@@ -1,19 +1,19 @@
-<!-- Enoncé : 
-Créer un formulaire sur la page index.php avec :  
+<!-- Enoncé :
+Créer un formulaire sur la page index.php avec :
 Une liste déroulante pour la civilité (Mr ou Mme)
 Un champ texte pour le nom
-Un champ texte pour le prénom 
-Ce formulaire doit rediriger vers la page index.php. 
+Un champ texte pour le prénom
+Ce formulaire doit rediriger vers la page index.php.
 Vous avez le choix de la méthode.-->
 
 <!-- Création de variables et insertion du header et de la barre de navigation. -->
 
 <?php
-    $titre= 'exercice5';
-    include 'header.php';
-    $lastNamePost = $_POST['lastName'] ?? '';
-    $firstNamePost = $_POST['firstName'] ?? '';
-    $civilityPost = $_POST['civility'] ?? '';
+$titre = 'exercice 5';
+include 'header.php';
+$lastNamePost = $_POST['lastName'] ?? '';
+$firstNamePost = $_POST['firstName'] ?? '';
+$civilityPost = $_POST['civility'] ?? '';
 ?>
 
 
@@ -24,18 +24,18 @@ Vous avez le choix de la méthode.-->
         <p class="font-weight-bold text-center text-uppercase">Formulaire de contact</p>
         <div class="form-group">
             <select class="custom-select" name="civility">
-                <option selected="selected">Civilité</option>
+                <option value='' disabled="disabled" selected="selected" hidden="hidden">Civilité</option>
                 <option value="Monsieur">Mr</option>
                 <option value="Madame">Mme</option>
             </select>
         </div>
         <div class="form-group">
-            <label for="formGroupExampleInput">Nom</label>
-            <input type="text" class="form-control" id="formGroupExampleInput" name="lastName" placeholder="Dupont">
+            <label for="lastName">Nom</label>
+            <input type="text" class="form-control" id="lastName" name="lastName" placeholder="Dupont">
         </div>
         <div class="form-group">
-            <label for="formGroupExampleInput2">Prénom</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" name="firstName" placeholder="Philippe">
+            <label for="firstName">Prénom</label>
+            <input type="text" class="form-control" id="firstName" name="firstName" placeholder="Philippe">
         </div>
         <button type="submit" class="btn btn-success">Valider</button>
     </form>
@@ -44,12 +44,12 @@ Vous avez le choix de la méthode.-->
 
 <!-- Affichage des inputs du formulaire. -->
 <div class="m-4">
-    <p><?php echo trim(htmlspecialchars($civilityPost)) ;?></p>
-    <p><?php echo trim(htmlspecialchars($lastNamePost)) ;?></p>
-    <p><?php echo trim(htmlspecialchars($firstNamePost)) ;?></p>
+    <p><?php echo trim(htmlspecialchars($civilityPost)); ?></p>
+    <p><?php echo trim(htmlspecialchars($lastNamePost)); ?></p>
+    <p><?php echo trim(htmlspecialchars($firstNamePost)); ?></p>
 </div>
 
 
 <!-- Insertion du footer. -->
 
-<?php include 'footer.php'; ?>
+<?php include 'footer.php';?>
