690f45b6d5f0f2142ca2fe1d33d29768a4f79157
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-27 21:13:16 +0200

    Partie 9 - Les dates - TP.

--- Partie 9 - Les dates/TP.php
+++ Partie 9 - Les dates/TP.php
@@ -1,6 +1,5 @@
 <!-- Enoncé : création d'un calendrier. -->
 
-<!-- Tuto : https://css-tricks.com/snippets/php/build-a-calendar-table/#usage -->
 
 <!-- Création de variables et insertion du header et de la barre de navigation. -->
 
@@ -8,113 +7,68 @@
 $titre = 'TP';
 include 'header.php';
 ?>
-
-
-<nav class= "navbar navbar-dark bg-dark mt-3 mb-3">
-    <a href="/TP.php" class="navbar-brand">Mon calendrier</a>
-</nav>
+<link rel="stylesheet" href="style.css">
 
 
 <?php
-
-
-function build_calendar($month, $year) {
-
-    // Création d'un tableau contenant les abbréviations des ours du mois.
-    $daysOfWeek = array('S', 'M', 'T', 'W', 'T', 'F', 'S');
-
-    // Recherche du premier jour du mois en question.
-    // mktime(hour, minute, second, month, day, year, is_dst).
-    $firstDayOfMonth = mktime(0, 0, 0, $month, 1, $year);
-
-    // Recherche du nombre de jours que le mois contient.
-    //'t' donne le nombre de jours dans le mois.
-    $numberDays = date('t', $firstDayOfMonth);
-
-    // Récupérer les informations du premier jour du mois en question.
-    // getdate(timestamp).
-    $dateComponents = getdate($firstDayOfMonth);
-
-    // Recherche du nom du mois en question.
-    $monthName = $dateComponents['mon'];
-
-    // Recherche de la valeur de l'index (0-6) du premier jour du mois en question.
-    $dayOfWeek = $dateComponents['wday'];
-
-    // Création du tableau.
-    $calendar = "<table class ='calendar'>";
-    $calendar .= "<caption>$monthName $year</caption>";
-    $calendar .= "<tr>";
-
-    // Création des headers du calendrier.
-    foreach ($daysOfWeek as $day) {
-        $calendar .= "<th class = 'header'>$day</th>";
-    }
-
-    // Création du reste du tableau.
-
-    // Initialisation du compteur de jour commençant par le 1°.
-    $currentDay = 1;
-    $calendar .= "</tr><tr>";
-
-    // La variable $dayOfWeeks est utilisée pour s'assurer que le calendrier comprend 7 colonnes.
-    if ($dayOfWeek > 0) {
-        $calendar .= "<td colspan= '$dayOfWeek'>&nbsp;</td>";
-    }
-
-    $month = str_pad($month, 2, "0", STR_PAD_LEFT);
-
-    while ($currentDay <= $numberDays) {
-
-        // 7° colonne (samedi) atteinte, démarrage d'une nouvelle ligne.
-
-        if ($dayOfWeek == 7) {
-
-            $dayOfWeek = 0;
-            $calendar .= "</tr><tr>";
-        }
-
-        $currentDayRel = str_pad($currentDay, 2, "0", STR_PAD_LEFT);
-
-        $date = "$year-$month-$currentDayRel";
-
-        $calendar .= "<td class='day' rel='$date'>$currentDay</td>";
-
-        // Incrémentation des compteurs.
-
-        $currentDay++;
-        $dayOfWeek++;
-
-    }
-
-// Remplissage de la ligne avec la dernière semaine du mois, si nécessaire.
-
-    if ($dayOfWeek != 7) {
-
-        $remainingDays = 7 - $dayOfWeek;
-        $calendar = "<td colspan= '$remainingDays'>&nbsp;</td>";
-    }
-
-    $calendar .= "</tr>";
-
-    $calendar .= "</table>";
-
-    return $calendar;
-
-}
-
+$titre = 'Caalendrier';
+$months = [1 => 'Janvier', 2 => 'Février', 3 => 'Mars', 4 => 'Avril', 5 => 'Mai', 6 => 'Juin', 7 => 'Juillet', 8 => 'Août', 9 => 'septembre', 10 => 'octobre', 11 => 'novembre', 12 => 'décembre'];
+$weekDays = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'];
+$selectedYear = $_GET['year'] ?? date('Y');
+$selectedMonthNumber = $_GET['month'] ?? date('n');
+$daysInMonth = cal_days_in_month(CAL_GREGORIAN, $selectedMonthNumber, $selectedYear);
+$firstDay = date('N' ,mktime(0, 0, 0, $selectedMonthNumber, 1, $selectedYear));
 ?>
 
-<?php
 
-    $dateComponents = getdate();
+<!-- creation du formulaire -->
+<form action="" method="get">
+    <label for="month">Mois :</label>
+    <select name="month" id="month">
+        <?php foreach ($months as $monthNumber => $month) : ?>
+            <option value="<?= $monthNumber ?>" <?= $selectedMonthNumber == $monthNumber ? 'selected' : ''; ?>>
+                <?= $month ?>
+            </option>
+        <?php endforeach; ?>
+    </select>
+    <label for="year">Année :</label>
+    <select name="year" id="year">
+        <?php for ($year = 1900; $year <= 2100; $year++) : ?>
+            <option value="<?= $year ?>" <?= $selectedYear == $year ? 'selected' : ''; ?>>
+                <?= $year ?>
+            </option>
+        <?php endfor; ?>
+    </select>
+    <input type="submit" value="valider">
+</form>
 
-    $month = $dateComponents['mon']; 			     
-    $year = $dateComponents['year'];
 
-    echo build_calendar($month, $year);
-
-?>
+<!-- affichage du tableau -->
+<table>
+    <thead>
+        <tr>
+            <?php foreach ($weekDays as $weekDay) : ?>
+                <td><?= $weekDay ?></td>
+            <?php endforeach; ?>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <?php 
+            $day = 1;
+            for($tile = 1; $tile <= $daysInMonth + $firstDay - 1; $tile++) :
+                if ($tile < $firstDay) { ?>
+                <td></td>
+                <?php } else { ?>
+                    <td><?= $day ?></td>
+                <?php $day++; }  
+                    if ($tile % 7 == 0) { ?>
+                    </tr><tr>
+                <?php }
+            endfor; ?>
+        </tr>
+    </tbody>
+</table>
 
 
 <!-- Insertion du footer. -->
