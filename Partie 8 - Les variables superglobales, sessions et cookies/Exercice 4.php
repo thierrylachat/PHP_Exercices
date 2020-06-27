26d6c90ff172fe4f5d3a78a198ce7c4fdd8de819
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-27 13:22:13 +0200

    Partie 8 - Les variables superglobales - Exercice 4 OK.

--- Partie 8 - Les variables superglobales, sessions et cookies/Exercice 4.php
+++ Partie 8 - Les variables superglobales, sessions et cookies/Exercice 4.php
@@ -1,16 +1,28 @@
-<!-- Enoncé : 
+<!-- Enoncé :
 Faire une page qui va récupérer les informations du cookie créé à l'exercice 3 et qui les affiche. -->
 
 
 <!-- Création de variables et insertion du header et de la barre de navigation. -->
 
 <?php
-    $titre= 'Exercice 4';
-    include 'header.php';
+$titre = 'Exercice 4';
+include 'header.php';
 ?>
 
+
+<!-- Récupération des données du cookie. -->
+
+<?php
+$login = $_COOKIE['login'] ?? '';
+$password = $_COOKIE['password'] ?? '';
+?>
+
+
+<!-- Affichage des données du cookie. -->
+
+<p>Votre identifiant est : <strong><?=$login ;?></strong> et votre de mot de passe est <strong><?= $password ;?></strong>.</p>
 
 
 <!-- Insertion du footer. -->
 
-<?php include 'footer.php'; ?>
+<?php include 'footer.php';?>
