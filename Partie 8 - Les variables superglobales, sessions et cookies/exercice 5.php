db6167a83b5568ad91a1bd26103c6b77cb6b7a6a
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-27 13:45:50 +0200

    Partie 8 - Les variables superglobales - Exercice 5 OK.

--- Partie 8 - Les variables superglobales, sessions et cookies/exercice 5.php
+++ Partie 8 - Les variables superglobales, sessions et cookies/exercice 5.php
@@ -1,55 +1,56 @@
-<!-- Enoncé : 
-Créer un formulaire sur la page index.php avec :  
-Une liste déroulante pour la civilité (Mr ou Mme)
-Un champ texte pour le nom
-Un champ texte pour le prénom 
-Ce formulaire doit rediriger vers la page index.php. 
-Vous avez le choix de la méthode.-->
+<!-- Enoncé :
+Faire une page qui va pouvoir modifier le contenu du cookie de l'exercice 3. -->
+
+
+<!-- Création du cookie à mettre AVANT le HTML impérativement. -->
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    // Vérification des données pour les cookies.
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+
+    // Stockage des données dans les cookies.
+    // setcookie envoie un cookie et sa synthaxe est setcookie(name, value, expire, path, domain, secure, httponly).
+    // Le / indique une portée générale pour le cookie.
+    setcookie('login', $login, time() + 365 * 24 * 3600, '/', '', false, false);
+    setcookie('password', $password, time() + 365 * 24 * 3600, '/', '', false, false);
+
+}
+?>
+
 
 <!-- Création de variables et insertion du header et de la barre de navigation. -->
 
 <?php
-    $titre= 'exercice5';
-    include 'header.php';
-    $lastNamePost = $_POST['lastName'] ?? '';
-    $firstNamePost = $_POST['firstName'] ?? '';
-    $civilityPost = $_POST['civility'] ?? '';
+$titre = 'Exercice 3';
+include 'header.php';
 ?>
 
 
-<!-- Création du formulaire avec la civilité, le nom et le prénom et utilisant la méthode post. -->
+<!-- Création du formulaire demandant le login et le mot de passe avec la méthode post. -->
 
 <div class="m-4 p-4 col-4 border border-secondary">
-    <form method="post" action="exercice5.php">
-        <p class="font-weight-bold text-center text-uppercase">Formulaire de contact</p>
+    <form method="post" action="">
+        <p class="font-weight-bold text-center text-uppercase">Formulaire d'identification</p>
         <div class="form-group">
-            <select class="custom-select" name="civility">
-                <option selected="selected">Civilité</option>
-                <option value="Monsieur">Mr</option>
-                <option value="Madame">Mme</option>
-            </select>
+            <label for="login">Login</label>
+            <input type="text" class="form-control" id="login" name="login" placeholder="M@teo21">
         </div>
         <div class="form-group">
-            <label for="formGroupExampleInput">Nom</label>
-            <input type="text" class="form-control" id="formGroupExampleInput" name="lastName" placeholder="Dupont">
-        </div>
-        <div class="form-group">
-            <label for="formGroupExampleInput2">Prénom</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" name="firstName" placeholder="Philippe">
+            <label for="password">Mot de passe</label>
+            <input type="text" class="form-control" id="password" name="password" placeholder="password">
         </div>
         <button type="submit" class="btn btn-success">Valider</button>
     </form>
 </div>
 
 
-<!-- Affichage des inputs du formulaire. -->
-<div class="m-4">
-    <p><?php echo trim(htmlspecialchars($civilityPost)) ;?></p>
-    <p><?php echo trim(htmlspecialchars($lastNamePost)) ;?></p>
-    <p><?php echo trim(htmlspecialchars($firstNamePost)) ;?></p>
-</div>
+<!-- Affichage des données du cookie. -->
+
+<?php var_dump($_COOKIE);?>
 
 
 <!-- Insertion du footer. -->
 
-<?php include 'footer.php'; ?>
+<?php include 'footer.php';?>
