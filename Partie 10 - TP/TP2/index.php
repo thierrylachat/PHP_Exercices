8b04f0154ff3e059b3f06cb5ecf96f4d0a345bb6
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-28 12:00:25 +0200

    Partie 10 - TP - TP2.

--- Partie 10 - TP/TP2/index.php
+++ Partie 10 - TP/TP2/index.php
@@ -1,54 +1,61 @@
 <?php
+
+// Initialisation des variables.
 $civility = $lastname = $firstname = $company = "";
 $age = 0;
 $errors = [];
 $isSubmitted = false;
+
+// Création des REGEX.
 $regexNames = '/^[a-zéèîïêëç]+((?:\-|\s)[a-zéèéîïêëç]+)?$/i';
 
+// Création du upload de la photo.
 $photo = $_COOKIE['picture'] ?? 'default.jpg';
 
+// Sounmission du formulaire.
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $isSubmitted = true;
+
+    // Vérification de l'existence des inputs. Nettoyage des inputs et vérification de la correspondance avec les REGEX.
+
+    // Civilité.
     $civility = filter_input(INPUT_POST, 'civility', FILTER_SANITIZE_NUMBER_INT);
     $filtercivility = filter_var($civility, FILTER_VALIDATE_INT, ["options" => ["min_range" => 1, "max_range" => 2]]);
     if (empty($civility)) {
         $errors['civility'] = 'Veuillez sélectionner un genre!';
-
-    } else if (!$filtercivility) { // équivaut écrire $filterCivility == 'false'
-        $errors['civility'] = 'Veuillez renseigner le champs!';
+    } else if (!$filtercivility) { // équivaut écrire $filtercivility == 'false'
+        $errors['civility'] = 'Veuillez renseigner le champ!';
     }
-    // valider l'âge
+
+    // Age.
     $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_NUMBER_INT);
     $filterAge = filter_var($age, FILTER_VALIDATE_INT, ["options" => ["min_range" => 14, "max_range" => 70]]);
     if (empty($age)) {
         $errors['age'] = 'Veuillez sélectionner un âge!';
-
     } else if (!$filterAge) { //
         $errors['age'] = 'Veuillez renseigner un âge valide!';
     }
 
-    // validation du firstname
-
+    // Prénom.
     $firstname = filter_input(INPUT_POST, 'firstname', FILTER_SANITIZE_STRING);
-    // vérifier la validité de la valeur
     if (empty($firstname)) {
-        $errors['firstname'] = 'Veuillez renseigner votre prenom';
+        $errors['firstname'] = 'Veuillez renseigner votre prénom.';
     } else if (!preg_match($regexNames, $firstname)) {
-        $errors['firstname'] = 'La valeur renseignée ne correspond pas au format attendu';
+        $errors['firstname'] = 'La valeur renseignée ne correspond pas au format attendu.';
     }
 
-    // validation du firstname
-
+    // Nom.
     $lastname = filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_STRING);
-    // vérifier la validité de la valeur
     if (empty($lastname)) {
-        $errors['lastname'] = 'Veuillez renseigner votre nom';
+        $errors['lastname'] = 'Veuillez renseigner votre nom.';
     } else if (!preg_match($regexNames, $lastname)) {
-        $errors['lastname'] = 'La valeur renseignée ne correspond pas au format attendu';
+        $errors['lastname'] = 'La valeur renseignée ne correspond pas au format attendu.';
     }
+
+    // Société.
     $company = filter_input(INPUT_POST, 'company', FILTER_SANITIZE_STRING);
     if (empty($company)) {
-        $errors['company'] = 'Veuillez renseigner votre société';
+        $errors['company'] = 'Veuillez renseigner votre société.';
     }
 
 }
@@ -56,13 +63,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 include 'header.php';
 ?>
 
+<!-- Création du formulaire. -->
 <div class="container">
     <form method="post" action="">
         <div class="row justify-content-center mt-5 ">
             <div class="col-7 bg-dark text-white p-3 ">
                 <div class="form-group">
                     <label for="civility">Civilité</label>
-                    <select name="civility" class="form-control <?=$isSubmitted && isset($errors['civility']) ? 'is-invalid' : ''?>" id="
+                    <select name="civility"
+                        class="form-control <?=$isSubmitted && isset($errors['civility']) ? 'is-invalid' : ''?>" id="
                         civility">
                         <option>Sélectionner votre civilité</option>
                         <option <?=$civility == 1 ? 'selected' : ''?> value="1">Monsieur</option>
@@ -72,22 +81,27 @@ include 'header.php';
                 </div>
                 <div class="form-group">
                     <label class="control-label" for="lastname">Nom</label>
-                    <input class="form-control <?=$isSubmitted && isset($errors['lastname']) ? 'is-invalid' : ''?>" value="<?=$lastname?>" id="lastname" name="lastname" type="text" placeholder="Lauper">
+                    <input class="form-control <?=$isSubmitted && isset($errors['lastname']) ? 'is-invalid' : ''?>"
+                        value="<?=$lastname?>" id="lastname" name="lastname" type="text" placeholder="Lauper">
                     <div class="invalid-feedback"><?=$errors['lastname'] ?? ""?></div>
                 </div>
                 <div class="form-group">
                     <label class="control-label" for="firstname">Prénom</label>
-                    <input class="form-control <?=$isSubmitted && isset($errors['firstname']) ? 'is-invalid' : ''?>" value="<?=$firstname?>" id="firstname" type="text" name="firstname" placeholder="Dave">
+                    <input class="form-control <?=$isSubmitted && isset($errors['firstname']) ? 'is-invalid' : ''?>"
+                        value="<?=$firstname?>" id="firstname" type="text" name="firstname" placeholder="Dave">
                     <div class="invalid-feedback"><?=$errors['firstname'] ?? ""?></div>
                 </div>
                 <div class="form-group has-success">
                     <label class="form-control-label " for="age">Age</label>
-                    <input type="number" value="<?=$age?>" name="age" class="form-control <?=$isSubmitted && isset($errors['age']) ? 'is-invalid' : ''?>" id=" age">
+                    <input type="number" value="<?=$age?>" name="age"
+                        class="form-control <?=$isSubmitted && isset($errors['age']) ? 'is-invalid' : ''?>" id=" age">
                     <div class="invalid-feedback"><?=$errors['age'] ?? ""?></div>
                 </div>
                 <div class="form-group has-danger">
                     <label class="form-control-label" for="company">Société</label>
-                    <input type="text" value="<?=$company?>" class="form-control <?=$isSubmitted && isset($errors['company']) ? 'is-invalid' : ''?>" name="company" id="company">
+                    <input type="text" value="<?=$company?>"
+                        class="form-control <?=$isSubmitted && isset($errors['company']) ? 'is-invalid' : ''?>"
+                        name="company" id="company">
                     <div class="invalid-feedback"><?=$errors['company'] ?? ""?></div>
                 </div>
                 <div class="text-center"> <button type="submit" class="btn btn-warning ">Envoyer!</button></div>
@@ -95,6 +109,8 @@ include 'header.php';
         </div>
     </form>
 </div>
+
+<!-- Validation du formulaire. -->
 <?php if ($isSubmitted && count($errors) == 0): ?>
 <div class="row justify-content-center mt-3">
     <div class="col-md-6">
@@ -109,7 +125,8 @@ include 'header.php';
                 </div>
                 <div class="col ">
                     <img class="img-fluid rounded" src="assets/img/<?=$photo?>" alt="<?=$photo?>">
-                    <p><a class="btn btn-outline-primary mt-2" href="upload.php">Télécharger une image de profile</a></p>
+                    <p><a class="btn btn-outline-primary mt-2" href="upload.php">Télécharger une image de profile</a>
+                    </p>
                 </div>
             </div>
         </div>
@@ -117,4 +134,5 @@ include 'header.php';
 </div>
 <?php
 endif;
+
 include 'footer.php';?>
